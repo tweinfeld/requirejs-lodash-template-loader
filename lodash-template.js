be17a40650ce46ae1e018ead3369c509f7a38de6
Tal Weinfeld <talw@webwise.co.il>
2015-04-11 01:49:38 +0300

    Added lodash to template function's scope

--- lodash-template.js
+++ lodash-template.js
@@ -3,14 +3,13 @@ define(["lodash", "text"], function (_, text) {
     "use strict";
 
     var buildMap = {};
-
     var persist = function (moduleName) {
         return function (payload) {
             return buildMap[moduleName] = payload;
         }
     };
 
-    var defineTemplate = _.template("define(\"<%-name%>\", function(){ return <%=content%> })");
+    var defineTemplate = _.template("define(\"<%-name%>\", [\"lodash\"], function(_){ return <%=content%> })");
 
     return {
         load: function (name, req, onLoadNative, config) {
